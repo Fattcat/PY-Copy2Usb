37e5b05383b069827895509509d8b4bdb4fa82a5
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-01-03 18:44:14 +0100

    Update Copy2usb.py

--- Copy2usb.py
+++ Copy2usb.py
@@ -1,26 +1,26 @@
 import os
 import shutil
 
-# Specify the source directory on your PC
-source_directory = "/path/to/source/directory"
+def copy_files(source_folder, target_folder, extensions):
+    for foldername, subfolders, filenames in os.walk(source_folder):
+        for filename in filenames:
+            if filename.endswith(tuple(extensions)):
+                source_path = os.path.join(foldername, filename)
+                target_path = os.path.join(target_folder, filename)
+                shutil.copy2(source_path, target_path)
+                print(f"Copied: {filename} from {source_path} to {target_folder}")
 
-# Specify the USB drive letter
-usb_drive_letter = "D:"
+# Get the current user's home directory
+user_home = os.path.expanduser('~')
 
-# Specify the destination folder on the USB drive
-destination_folder = "elezz"
+# Specify the source directories including the desktop and Downloads folder
+source_directories = [os.path.join(user_home, 'Desktop'), os.path.join(user_home, 'Downloads')]
+# Specify the destination folder on your USB drive
+target_directory = 'E:/CopyedFromPC'
+os.makedirs(target_directory, exist_ok=True)
+# List of file extensions to search for
+file_extensions = [".txt", ".pdf", ".gif", ".jpg", ".png", ".jpeg", ".docs", ".dox", ".mp3", ".mp4", ".wav"]
 
-# Create the destination folder on the USB drive if it doesn't exist
-usb_destination_path = os.path.join(usb_drive_letter, destination_folder)
-os.makedirs(usb_destination_path, exist_ok=True)
-
-# Get a list of all .txt and .pdf files in the source directory
-files_to_copy = [file for file in os.listdir(source_directory) if file.endswith(('.txt', '.pdf'))]
-
-# Copy each file to the USB drive
-for file_name in files_to_copy:
-    source_path = os.path.join(source_directory, file_name)
-    destination_path = os.path.join(usb_destination_path, file_name)
-    shutil.copy2(source_path, destination_path)
-
-print("Files copied successfully.")
+# Iterate over each source directory
+for source_directory in source_directories:
+    copy_files(source_directory, target_directory, file_extensions)
